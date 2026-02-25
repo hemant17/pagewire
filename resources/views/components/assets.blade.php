8fef186a1dd648809d8a6e9d3a300b4ce997d8bc
hemant17 <hemantbhardwaj17@gmail.com>
2026-02-26 00:34:16 +0530

    added editor support

--- resources/views/components/assets.blade.php
+++ resources/views/components/assets.blade.php
@@ -17,5 +17,77 @@
             <script src="{{ $src }}"></script>
         @endif
     @endforeach
+
+    <script>
+        (() => {
+            if (!window.toolbarOptions) {
+                window.toolbarOptions = [
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['blockquote', 'code-block'],
+                    ['table-better'],
+                    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+                    [{ 'script': 'sub'}, { 'script': 'super' }],
+                    [{ 'indent': '-1'}, { 'indent': '+1' }],
+                    [{ 'direction': 'rtl' }],
+                    [{ 'size': ['small', false, 'large', 'huge'] }],
+                    [{ 'color': [] }, { 'background': [] }],
+                    [{ 'font': [] }],
+                    [{ 'align': [] }],
+                    ['link', 'image', 'video', 'formula'],
+                    ['clean']
+                ];
+            }
+
+            if (!window.settings) {
+                window.settings = {
+                    theme: 'snow',
+                    modules: {
+                        resize: {
+                            modules: ['DisplaySize', 'Toolbar', 'Resize', 'Keyboard'],
+                            keyboardSelect: true,
+                            selectedClass: 'selected',
+                            activeClass: 'active',
+                            embedTags: ['VIDEO', 'IFRAME'],
+                            tools: ['left', 'center', 'right', 'full', 'edit'],
+                            parchment: {
+                                image: {
+                                    attribute: ['width'],
+                                    limit: {
+                                        minWidth: 100
+                                    }
+                                },
+                                video: {
+                                    attribute: ['width', 'height'],
+                                    limit: {
+                                        minWidth: 200,
+                                        ratio: 0.5625
+                                    }
+                                }
+                            },
+                        },
+                        toolbar: {
+                            container: window.toolbarOptions,
+                            handlers: {
+                                image: function() {
+                                    selectLocalImage();
+                                }
+                            }
+                        },
+                        'table-better': {
+                            language: 'en_US',
+                            menus: ['column', 'row', 'merge', 'table', 'cell', 'wrap', 'copy', 'delete'],
+                            toolbarTable: true
+                        },
+                    }
+                };
+            }
+
+            if (!window.__quillRegistered) {
+                Quill.register({'modules/table-better': QuillTableBetter}, true);
+                window.__quillRegistered = true;
+            }
+        })();
+    </script>
 @endonce
 
